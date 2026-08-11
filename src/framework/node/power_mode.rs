86280c1b4bae1595427c5ba53389e317e9dae4d4
Allenwdk <15620209105@163.com>
2026-08-11 16:22:26 +0800

    ci-fix: remove duplicate Mode import in power_mode.rs

--- src/framework/node/power_mode.rs
+++ src/framework/node/power_mode.rs
@@ -1,5 +1,4 @@
 use super::Node;
-use crate::framework::{config::Mode, error::Result};
 
 // Mode is defined in crate::framework::config::data::Mode.
 // This module exists only for backwards compatibility with existing code that
@@ -10,7 +9,7 @@ impl Node {
     /// Get the current power mode.
     /// Since fas-rs is now independent of external schedulers, this always returns Balance.
     /// The actual mode is determined by Config::default_mode().
-    pub fn get_mode(&mut self) -> Result<Mode> {
+    pub fn get_mode(&mut self) -> crate::framework::error::Result<Mode> {
         Ok(Mode::Balance)
     }
 }
